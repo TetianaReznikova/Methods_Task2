da1a92deba58efb22578455183051f74d617a9ef
TetianaReznikova <reznikovat2705@gmail.com>
2023-09-22 19:44:21 +0300

    Initial commit

---
+++ src/task/Task.java
@@ -0,0 +1,60 @@
+package task;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Task {
+    static double amountOfPurchase;
+    static double discountAmount;
+    static double totalAmount;
+
+
+    public static void main(String[] args) {
+        amountOfPurchase = getSum();
+        discountAmount = discount(amountOfPurchase);
+        totalAmount = sumForPay(amountOfPurchase, discountAmount);
+        dataOnScreen(amountOfPurchase, discountAmount, totalAmount);
+    }
+
+    //введення суми купівль
+    private static double getSum() {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
+        System.out.println("Enter sum: ");
+        double amount = sc.nextDouble();
+        sc.close();
+        return amount;
+    }
+
+    //розрахунок суми дисконту
+    private static double discount(double sum) {
+        if (sum <= 5000) {
+            return sum * 0.05;
+        } else if (sum <= 10000) {
+            return sum * 0.1;
+        } else {
+            return sum * 0.15;
+        }
+    }
+
+    //розрахунок суми за мінусом дисконту
+    private static double sumForPay(double calculatedValue, double discValue) {
+        return roundSum(calculatedValue - discValue);
+    }
+
+    //округлення до 2х знаків
+    public static double roundSum(double value) {
+        return new BigDecimal(value).setScale(2, RoundingMode.HALF_UP).doubleValue();
+    }
+
+    //виведення результату на екран
+    public static void dataOnScreen(double sum, double discountSum, double total) {
+        System.out.print("Total coust: " + sum);
+        System.out.print("Your discount is: " + discountSum);
+        System.out.print("You have to pay: " + total);
+    }
+}
+
+
