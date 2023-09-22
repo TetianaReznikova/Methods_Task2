c2955fa0bb87c7bfeed42d68996b97dc98a959c9
TetianaReznikova <reznikovat2705@gmail.com>
2023-09-22 20:31:00 +0300

    amendment in calcul of discount

--- src/task/Task.java
+++ src/task/Task.java
@@ -31,11 +31,11 @@ public class Task {
     //розрахунок суми дисконту
     private static double discount(double sum) {
         if (sum <= 5000) {
-            return sum * 0.05;
+            return roundSum (sum * 0.05);
         } else if (sum <= 10000) {
-            return sum * 0.1;
+            return roundSum (sum * 0.1);
         } else {
-            return sum * 0.15;
+            return roundSum (sum * 0.15);
         }
     }
 
